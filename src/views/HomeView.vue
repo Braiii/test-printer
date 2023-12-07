aaa10e5a0d1dfa2adefb1a451f3efa3459182d4c
Brainard <delacruzbrainard15@gmail.com>
2023-12-07 17:37:31 +0800

    simple receipt design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>RBCF Trading</h1>
+    <hr>
+    <h2>Products</h2>
+    <ul>
+      <li>Chelsie - 100 - 1000</li>
+    </ul>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+}
+
+h2 {
+  font-size: 2rem;
+}
+
+ul li {
+  font-size: 1.5rem;
+}
+
+</style>
